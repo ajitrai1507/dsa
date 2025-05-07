2773cb20e9824f7c1da21a9a337d1ac5441a7795
Ajit Rai <105290694+ajitrai1507@users.noreply.github.com>
2025-05-07 13:28:52 +0530

    Pattern 7

---
+++ 29.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    char ch;
+
+    for (int i = 1; i <= 5; i++)
+    {
+        for (int j = 1; j <= 5; j++)
+        {
+            ch = 'a' + i - 1;
+            cout << ch << " ";
+        }
+        cout << endl;
+    }
+}
