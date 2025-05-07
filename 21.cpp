6f2bf17fbb8c032dd8c3b8644def92aafd9b087a
Ajit Rai <105290694+ajitrai1507@users.noreply.github.com>
2025-05-07 12:28:01 +0530

    Factorial of a number

---
+++ 21.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num, sum = 1;
+    cout << "Enter the number" << endl;
+    cin >> num;
+
+    for (int i = 1; i <= num; i++)
+    {
+        sum = sum * i;
+    }
+    cout << sum;
+}
